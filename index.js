630c0ac2725d22f0d25756c27ec6ce469e7025a8
Jeongin Yoon <jy3524@nyu.edu>
2021-11-02 08:59:49 -0400

    Start setting up firebase

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const express = require('express');
+// Initiate Express
 const app = express();
-
+// Setting port - dynamically with Heroku
 const port = process.env.PORT || 4000;
 
+// Import Firebase
+const firebase = require('firebase/app');
+// Get configuration object so we can communicate with Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDxpEEi5dzYixGpDJ5EJZnSWaabC3kpIWc",
+  authDomain: "exercise-four-f21-jeongin.firebaseapp.com",
+  projectId: "exercise-four-f21-jeongin",
+  storageBucket: "exercise-four-f21-jeongin.appspot.com",
+  messagingSenderId: "736248090673",
+  appId: "1:736248090673:web:e43456b05ce8c648d1c510"
+};
+//Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Routes for directing user to correct place
 const indexRoute = require('./routes/index');
 const articleRoute = require('./routes/article');
 const createArticleRoute = require('./routes/createArticle');
-
+// Tell express to use routes...
 app.use('/', indexRoute);
 app.use('/article', articleRoute);
 app.use('/create', createArticleRoute);
-
+// Listen for requests on the port
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,19 @@
 const express  = require('express');
 const router = express.Router();
+// Require Firebase
+const firebase = require('firebase/firestore/lite'); // Fix this
+// Initialize Firestore Database
+const db = firebase.getFirestore(); // Fix this
+// Reference to the blogposts collection
+const blogposts = db.collection('blogposts'); // Fix this
 
-// TODO: Hook up firebase, get all articles
 // Get all articles from firebase
 router.get('/', (req, res) => {
-  res.send(`
-  <h1>All Articles</h1>
-  <p>Articles coming soon...</p>
-  `)
+  // Create empty array
+  const blogpostsArray = [];
+  // ToDo: get blogposts JSON from Firebase...
+  // Push document from blogposts into the blogposts array...
+  res.send(blogpostsArray);
 });
 
 module.exports = router;
